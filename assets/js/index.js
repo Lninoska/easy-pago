d41ead1f6b2b42a0a13a18b6763fb64dd961a8c0
Lorena <lorena.n.barrioz@gmail.com>
2024-10-16 23:57:06 -0300

    firts commits

---
+++ assets/js/index.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    $('#calcular').click(function(){
+        
+    let monto = $('#monto')[0].valueAsNumber;
+    let cuotas = $('#cuotas')[0].valueAsNumber;
+    let interes = $('#interes')[0].valueAsNumber/ 100;
+
+    if(isNaN(monto)|| isNaN(cuotas) || isNaN(interes) || cuotas <= 0) {
+        $('#resultado').html('<span style ="color: #5543ca;">Por favor, ingresa valores validos </span>')
+        return;
+    }
+    let interestotal = monto * interes * cuotas;
+
+    let montofinal = monto + interestotal;
+
+    let valorcuota = montofinal/cuotas;
+
+    let nuevovalorcuota = valorcuota.toLocaleString('es-CL', {style: 'currency', currency: 'CLP'});
+    let nuevomontofinal = montofinal.toLocaleString('es-CL', { style: 'currency', currency: 'CLP'});
+    $('#result').html(`<p>¡No te preocupes puedes pagarlo en coutas ${cuotas} de ${nuevovalorcuota} </p>`)
+    $('#total').html(`El monto total a pagar será ${nuevomontofinal}`)
+    })
+})
+
+
+
+
+
+
